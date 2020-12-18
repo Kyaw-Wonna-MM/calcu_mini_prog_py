63c77b6c2f3bb188a072ac3006ac09c20a0cde08
Kyaw-Wonna-MM <kyaw.wonna3991@gmail.com>
2020-12-18 16:37:03 +0630

    "sign unsign integer def"

--- binary_01.py
+++ binary_01.py
@@ -12,32 +12,36 @@ signed integer = 31 bit (1 bit for + or - sign)
 
 '''
 
-count = 0
-iBit  = None
 
-while True:
-    total_bits = input('Enter bit: ')
-    print(f'bits : {total_bits}')
-    print("-----------------------")
-   
-    if total_bits == "end":
-        print("End Program")
-        break
-    else:
-        try:
-            iBit = int(total_bits)
-        except:
-            print("Only Numeric!")
-            continue
+def Calcu_inte():
+    count = 0
+    iBit  = None
+    while True:
+        total_bits = input('Enter bit: ')
+        print(f'bits : {total_bits}')
+        print("-----------------------")
     
-    if iBit == 31:
-        print("Signed integer")  
-    elif iBit == 8:
-        print("8 bit")    
-    if iBit != None:
-        start = -(pow(2 , iBit))
-        end = (pow(2 , iBit)) - 1
-        print(f' Signed value is: {start} to {end}')
-    count = count + 1
+        if total_bits == "end":
+            print("End Program")
+            break
+        else:
+            try:
+                iBit = int(total_bits)
+            except:
+                print("Only Numeric!")
+                continue
+        
+        if iBit == 31:
+            name = "Sign Integer"
+            print("Signed integer")  
+        elif iBit == 8:
+            name = "8 Bit"
+            print("8 Bitt")    
+        if iBit != None:
+            start = -(pow(2 , iBit))
+            end = (pow(2 , iBit)) - 1
+            print(f' Signed {name} value is: {start} to {end}')
+        count = count + 1
 
         
+Calcu_inte()
